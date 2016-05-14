7a7df0cb28ff0ddac1084a7d58e4f303ea5cdfb7
imessage357 <imessage357@gmail.com>
2016-05-14 16:48:29 -0400

    Update Everything.ps1

--- Everything.ps1
+++ Everything.ps1
@@ -1,7 +1,1 @@
-$app = 'everything'
-$Source = "https://www.myget.org/F/logs2424/api/v2"
- if (Test-Path -Path 'C:\ProgramData\chocolatey') { Remove-Item -Path 'C:\ProgramData\chocolatey' -Recurse -force }
- iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
- cls
-$env:path = "$($env:ALLUSERSPROFILE)\chocolatey\bin;$($env:Path)"
-cinst  $app -source $Source --f -y 
+ choco install procexp --f -y 
